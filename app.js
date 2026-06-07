64513b95585085a3174d0aace9a37efb026d0541
July Beiner <july.jarabeiner@gmail.com>
2026-06-07 16:25:28 +0000

    Add currency conversion tests and functions

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = (valueInDollar / 1.07) * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = (valueInYen / 156.5 ) * 0.87;
+    return valueInPound;
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
---
+++ test.js
@@ -0,0 +1,38 @@
+// Import the function sum from the app.js file
+const { sum } = require('./app.js');
+
+// Start your first test
+test('adds 14 + 9 to equal 23', () => {
+    // Inside the test we call our sum function with 2 numbers
+    let total = sum(14, 9);
+
+    // We expect the sum of those 2 numbers to be 23
+    expect(total).toBe(23);
+});
+
+test("Euro to dollar", function() {
+    const { fromEuroToDollar } = require('./app.js');
+    const dollars = fromEuroToDollar(3.5);
+    const expected = 3.5 * 1.07; 
+
+     expect(fromEuroToDollar(3.5)).toBe(3.745); 
+})
+
+test("Dollar to Yen", function() {
+    const { fromDollarToYen } = require('./app.js');
+    const yens = fromDollarToYen(8.1);
+    const expected = (8.1 / 1.07) * 156.5;
+
+    expect(fromDollarToYen(8.1)).toBe(expected);
+});
+
+test("Yen to Pound", function() {
+    const { fromYenToPound } = require('./app.js');
+    const pounds = fromYenToPound(8.1);
+    const expected = (8.1 / 156.5) * 0.87;
+
+    expect(fromYenToPound(8.1)).toBe(expected);
+});
+
+
+
